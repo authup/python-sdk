e1581e719490316c58723cc0054b7f324ce1ba93
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2024-01-08 14:31:58 +0100

    fix: Implement and fix realm test

--- authup/tests/client/test_realm_client.py
+++ authup/tests/client/test_realm_client.py
@@ -4,8 +4,8 @@ import sys
 
 import pytest
 
-from authup.domains.clients.resource_clients import RealmClient
-from authup.domains.schemas import Realm, RealmCreate, RealmUpdate
+from authup.domains.clients.resource_clients.realm import RealmClient
+from authup.domains.schemas.realm import Realm, RealmCreate, RealmUpdate
 
 if (
     sys.version_info[0] == 3
@@ -26,35 +26,38 @@ async def test_realm_get_many(realm_client):
     assert realms
     assert isinstance(realms[0], Realm)
 
-    print(realm_client.get_many)
-
-    # sync_realms = realm_client.get_many.sync()
+    print(f"\n{[r.id for r in await realm_client.get_many()]}")
 
 
 @pytest.mark.asyncio
 async def test_realm_get_one(realm_client):
     realm = await realm_client.create(RealmCreate(name=os.urandom(8).hex()))
-    test_realm = await realm_client.get(realm.id)
+    test_realm = await realm_client.get_one(realm.id)
     assert realm
     assert isinstance(realm, Realm)
     assert realm.id == test_realm.id
     assert realm.name == test_realm.name
 
-    # sync_realm = realm_client.get_one.sync("default")
-
 
 @pytest.mark.asyncio
 async def test_realm_create(realm_client):
-    test_realm = RealmCreate(name=os.urandom(8).hex())
+    realm_name = os.urandom(8).hex()
+    test_realm = RealmCreate(name=realm_name)
     realm = await realm_client.create(test_realm)
     assert realm
     assert isinstance(realm, Realm)
     assert realm.name == test_realm.name
+    print(f"\nID: {realm.id}")
+    print(f"\nName: {realm.name}")
+    print(
+        f"\nDatetime:\n\tCreated: {test_realm.created_at}\n\tUpdated: {test_realm.updated_at}"
+    )
 
 
 @pytest.mark.asyncio
 async def test_realm_update(realm_client):
-    realm = await realm_client.create(RealmCreate(name=os.urandom(8).hex()))
+    realm_name = os.urandom(8).hex()
+    realm = await realm_client.create(RealmCreate(name=realm_name))
 
     updated_name = os.urandom(8).hex()
     test_realm_updated = RealmUpdate(name=updated_name)
@@ -63,10 +66,22 @@ async def test_realm_update(realm_client):
     assert updated_realm
     assert isinstance(updated_realm, Realm)
     assert updated_realm.name == updated_name
+    print(f"\nID: {realm.id}")
+    print(f"\nName:\n\tOriginal: {realm.name}\n\tUpdated: {updated_realm.name}")
+    print(
+        f"\nDatetime:\n\tCreated: {test_realm_updated.created_at}\n\tUpdated: {test_realm_updated.updated_at}"
+    )
 
 
 @pytest.mark.asyncio
 async def test_realm_delete(realm_client):
-    realm = await realm_client.create(RealmCreate(name=os.urandom(8).hex()))
+    realm_id = os.urandom(8).hex()
+    realm = await realm_client.create(RealmCreate(name=realm_id))
     deleted_id = await realm_client.delete(realm.id)
+    print(f"\nRealm generated: id={realm.id}")
+    print(f"Delete realm with id={deleted_id}")
+    print(
+        f"Deleted id in list of current realms: {deleted_id in [r.id for r in await realm_client.get_many()]}"
+    )
     assert deleted_id == realm.id
+    assert deleted_id not in [r.id for r in await realm_client.get_many()]
