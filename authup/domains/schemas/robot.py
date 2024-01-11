99ddc095cab7ec1a41f638839cbe13e268d95e7f
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2024-01-11 15:21:41 +0100

    feat: Add robot resource including tests

---
+++ authup/domains/schemas/robot.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+from ..constants import DomainType
+from ..types_base import DomainEventBaseContext
+from .realm import Realm
+
+
+class Robot(BaseModel):
+    id: str
+    secret: Optional[str]
+    name: str
+    description: Optional[str]
+    active: bool = False
+    created_at: str = datetime.now().isoformat()
+    updated_at: str = datetime.now().isoformat()
+    user_id: Optional[str]
+    user: Optional[str]
+    realm_id: str
+    realm: Optional[Realm]
+
+
+class RobotEventContext(DomainEventBaseContext):
+    type: str = f"{DomainType.ROBOT}"
+    data: Robot
+
+
+class RobotCreate(Robot):
+    id: Optional[str]
+    realm_id: Optional[str]
+    pass
+
+
+class RobotUpdate(Robot):
+    id: Optional[str]
+    realm_id: Optional[str]
+    updated_at: str = datetime.now().isoformat()
+    pass
+
+
+ROBOT_SYSTEM_NAME = "SYSTEM"
---
+++ authup/tests/client/resource_clients/test_robot_client.py
@@ -0,0 +1,112 @@
+import asyncio
+import os
+import sys
+
+import pytest
+
+from ....domains.clients.resource_clients.realm import RealmClient
+from ....domains.clients.resource_clients.robot import RobotClient
+from ....domains.schemas.realm import Realm, RealmCreate
+from ....domains.schemas.robot import Robot, RobotCreate, RobotUpdate
+
+if (
+    sys.version_info[0] == 3
+    and sys.version_info[1] >= 8
+    and sys.platform.startswith("win")
+):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+@pytest.fixture
+def robot_client(authup_client):
+    return RobotClient(Robot, authup_client.http, prefix="robots")
+
+
+@pytest.fixture
+def realm_client(authup_client):
+    return RealmClient(Realm, authup_client.http, prefix="realms")
+
+
+@pytest.mark.asyncio
+async def test_robot_get_many(robot_client):
+    robots = await robot_client.get_many()
+    assert robots
+    assert isinstance(robots[0], Robot)
+
+    print(f"\n{[r.id for r in await robot_client.get_many()]}")
+
+
+@pytest.mark.asyncio
+async def test_robot_get_one(robot_client, realm_client):
+    realm = await realm_client.create(RealmCreate(name=os.urandom(8).hex()))
+    robot = await robot_client.create(
+        RobotCreate(name=os.urandom(8).hex(), secret="test", realm_id=realm.id)
+    )
+    test_robot = await robot_client.get_one(robot.id)  # , '?include=realm')
+    assert robot
+    assert isinstance(robot, Robot)
+    assert robot.id == test_robot.id
+    assert robot.name == test_robot.name
+
+
+@pytest.mark.asyncio
+async def test_robot_create(robot_client, realm_client):
+    realm = await realm_client.create(RealmCreate(name=os.urandom(8).hex()))
+    test_robot = RobotCreate(name=os.urandom(8).hex(), secret="test", realm_id=realm.id)
+    robot = await robot_client.create(test_robot)
+    assert robot
+    assert isinstance(robot, Robot)
+    assert robot.name == test_robot.name
+    print(f"\nID: {robot.id}")
+    print(f"\nName: {robot.name}")
+    print(
+        f"\nDatetime:\n\tCreated: {test_robot.created_at}\n\tUpdated: {test_robot.updated_at}"
+    )
+
+
+@pytest.mark.asyncio
+async def test_robot_update(robot_client, realm_client):
+    realm = await realm_client.create(RealmCreate(name=os.urandom(8).hex()))
+    robot = await robot_client.create(
+        RobotCreate(name=os.urandom(8).hex(), secret="test", realm_id=realm.id)
+    )
+    updated_name = os.urandom(8).hex()
+    test_robot_updated = RobotUpdate(
+        name=updated_name, secret="test", realm_id=realm.id
+    )
+    updated_robot = await robot_client.update(robot.id, test_robot_updated)
+
+    assert updated_robot
+    assert isinstance(updated_robot, Robot)
+    assert updated_robot.name == updated_name
+    print(f"\nID: {robot.id}")
+    print(f"\nName:\n\tOriginal: {robot.name}\n\tUpdated: {updated_robot.name}")
+    print(
+        f"\nDatetime:\n\tCreated: {test_robot_updated.created_at}\n\tUpdated: {test_robot_updated.updated_at}"
+    )
+
+
+@pytest.mark.asyncio
+async def test_robot_delete(robot_client, realm_client):
+    realm = await realm_client.create(RealmCreate(name=os.urandom(8).hex()))
+    robot = await robot_client.create(
+        RobotCreate(name=os.urandom(8).hex(), secret="test", realm_id=realm.id)
+    )
+    deleted_id = await robot_client.delete(robot.id)
+    print(f"\nRobot generated: id={robot.id}")
+    print(f"Delete robot with id={deleted_id}")
+    print(
+        f"Deleted id in list of current robots: {deleted_id in [r.id for r in await robot_client.get_many()]}"
+    )
+    assert deleted_id == robot.id
+    assert deleted_id not in [r.id for r in await robot_client.get_many()]
+
+
+# @pytest.mark.asyncio
+# async def test_robot_integrity(robot_client, realm_client):
+#     realm = await realm_client.create(RealmCreate(name=os.urandom(8).hex()))
+#     robot = await robot_client.create(RobotCreate(name=os.urandom(8).hex(),
+#                                                   secret='test',
+#                                                   realm_id=realm.id))
+#     test_integrity = await robot_client.integrity(robot.id)
+#     print('int:', test_integrity)
